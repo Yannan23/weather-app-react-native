d452b1e2115fbff0fef22fb55c2f5942765293d8
Yannan23 <yannansun0723@gmail.com>
2025-03-29 04:52:28 +1100

    add toggle button

--- src/components/Header/components/SeachBar/SearchBar.jsx
+++ src/components/Header/components/SeachBar/SearchBar.jsx
@@ -1,17 +1,26 @@
 import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import { CalendarDaysIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline'
-// import theme from '../../../../theme'
+import { MagnifyingGlassIcon } from 'react-native-heroicons/outline'
 
 const SearchBar = () => {
+    const [city, setCity] = useState('')
+    const [showSearch, toggleShowSearch] = useState(true)
+
     return (
-        <Container>
-            <InputText placeholder='Search City' />
-            <MagnifyingIconContainer>
-                <MagnifyingGlassIcon color='white' size='25px' />
-            </MagnifyingIconContainer>
-        </Container>
+        <>
+
+            <Container>
+                {
+                    showSearch ? (<InputText placeholder='Search City' placeholderTextColor={'lightgray'} />) : null
+                }
+
+                <MagnifyingIconContainer onPress={() => toggleShowSearch(!showSearch)} >
+                    <MagnifyingGlassIcon color='white' size='25px' />
+                </MagnifyingIconContainer>
+            </Container >
+        </>
+
     )
 }
 
@@ -19,18 +28,27 @@ const Container = styled.View`
     color: white;
     border: 1px solid white;
     border-radius: 32px;
-    padding:12px 16px;
+    padding-left: 16px;
     margin: 8px 8px;
+    display: flex;
+    flex-direction: row;
+    justify-content:flex-end;
+    align-items: center;
 `
 
 const InputText = styled.TextInput`
     color: white;
     font-size: 24px;
+    flex-grow: 1;
 `
 
 const MagnifyingIconContainer = styled.TouchableOpacity`
     background-color: ${(props) => props.theme.bgWhite(0.3)}; /* Applying bgWhite function with 0.3 opacity */
-
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    padding: 12px;
+    margin:4px;
 `
 
 export default SearchBar
