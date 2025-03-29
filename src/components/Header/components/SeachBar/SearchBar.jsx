77d633c2025f1519f2945fa82d5610f262a79a43
Yannan23 <yannansun0723@gmail.com>
2025-03-29 17:38:33 +1100

    add google font

--- src/components/Header/components/SeachBar/SearchBar.jsx
+++ src/components/Header/components/SeachBar/SearchBar.jsx
@@ -1,10 +1,21 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Image, Text, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import styled, { css } from 'styled-components'
-import { MagnifyingGlassIcon } from 'react-native-heroicons/outline'
+import { MagnifyingGlassIcon, ArrowUpRightIcon } from 'react-native-heroicons/outline'
+import { useFonts } from 'expo-font'
 
 const SearchBar = () => {
     const [showSearch, toggleShowSearch] = useState(false)
+
+    const [fontsloaded] = useFonts({
+        'Saira_Expanded-SemiBold': require('../../../../assets/fonts/Saira_Expanded-SemiBold.ttf'),
+        'Saira_SemiExpanded-Light': require('../../../../assets/fonts/Saira_SemiExpanded-Light.ttf'), // Ensure case matches
+
+    })
+
+    if (!fontsloaded) {
+        return undefined
+    }
 
     return (
         <>
@@ -12,7 +23,16 @@ const SearchBar = () => {
                 {
                     showSearch ? (
                         <InputText placeholder='Search City' placeholderTextColor={'lightgray'} />
-                    ) : null
+                    ) : (
+                        <View>
+                            <CityContainer>
+                                <City>Jakata</City>
+                                <ArrowUpRightIcon size={15} color='white' />
+                            </CityContainer>
+                            <Greeting>Good Morning</Greeting>
+                        </View>
+                    )
+
                 }
                 <MagnifyingIconContainer onPress={() => toggleShowSearch(!showSearch)}>
                     <MagnifyingGlassIcon color='white' size='25px' />
@@ -23,6 +43,27 @@ const SearchBar = () => {
     )
 }
 
+const CityContainer = styled.View`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
+
+const City = styled.Text`
+    color: white;
+    font-family:'Saira_SemiExpanded-Light';
+    font-size: 20px;
+    margin-left:4px;
+    margin-right:4px;
+`
+
+const Greeting = styled.Text`
+    color: white;
+    margin-left:4px;
+    font-size: 24px;
+    font-family: 'Saira_Expanded-SemiBold';
+`
+
 const Container = styled.View`
     color: white;
     border-radius: 32px;
@@ -30,13 +71,17 @@ const Container = styled.View`
     margin: 8px 8px;
     display: flex;
     flex-direction: row;
-    justify-content:flex-end;
-    align-items: center;
+    align-items: top;
     ${(props) => {
         switch (props.variant) {
             case true:
                 return css`
                     border: 1px solid white;
+                    justify-content:flex-end;
+                `;
+            case false:
+                return css`
+                    justify-content:space-between;
                 `
         }
     }}
