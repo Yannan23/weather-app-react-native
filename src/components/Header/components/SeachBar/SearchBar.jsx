57907e10f7ce4d12d7d9b8521e7ed045a3a5f6a7
Yannan23 <yannansun0723@gmail.com>
2025-03-29 12:36:05 +1100

    toggle button fully syled

--- src/components/Header/components/SeachBar/SearchBar.jsx
+++ src/components/Header/components/SeachBar/SearchBar.jsx
@@ -1,21 +1,20 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { MagnifyingGlassIcon } from 'react-native-heroicons/outline'
 
 const SearchBar = () => {
-    const [city, setCity] = useState('')
-    const [showSearch, toggleShowSearch] = useState(true)
+    const [showSearch, toggleShowSearch] = useState(false)
 
     return (
         <>
-
-            <Container>
+            <Container variant={showSearch} >
                 {
-                    showSearch ? (<InputText placeholder='Search City' placeholderTextColor={'lightgray'} />) : null
+                    showSearch ? (
+                        <InputText placeholder='Search City' placeholderTextColor={'lightgray'} />
+                    ) : null
                 }
-
-                <MagnifyingIconContainer onPress={() => toggleShowSearch(!showSearch)} >
+                <MagnifyingIconContainer onPress={() => toggleShowSearch(!showSearch)}>
                     <MagnifyingGlassIcon color='white' size='25px' />
                 </MagnifyingIconContainer>
             </Container >
@@ -26,7 +25,6 @@ const SearchBar = () => {
 
 const Container = styled.View`
     color: white;
-    border: 1px solid white;
     border-radius: 32px;
     padding-left: 16px;
     margin: 8px 8px;
@@ -34,6 +32,14 @@ const Container = styled.View`
     flex-direction: row;
     justify-content:flex-end;
     align-items: center;
+    ${(props) => {
+        switch (props.variant) {
+            case true:
+                return css`
+                    border: 1px solid white;
+                `
+        }
+    }}
 `
 
 const InputText = styled.TextInput`
@@ -43,7 +49,7 @@ const InputText = styled.TextInput`
 `
 
 const MagnifyingIconContainer = styled.TouchableOpacity`
-    background-color: ${(props) => props.theme.bgWhite(0.3)}; /* Applying bgWhite function with 0.3 opacity */
+    background-color: ${(props) => props.theme.bgWhite(0.2)};
     width: 50px;
     height: 50px;
     border-radius: 50%;
