36733ad9129eedd9d6c5c074b9c4a43d324397c6
Yannan23 <yannansun0723@gmail.com>
2025-04-03 16:02:22 +1100

    restructured current weather page

--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -1,18 +1,47 @@
-import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native'
+import { Text, View, TouchableOpacity, ScrollView } from 'react-native'
 import React, { useState } from 'react'
-import styled, { useTheme } from 'styled-components/native'
+import styled, { css, useTheme } from 'styled-components/native'
 import sun from '../../assets/images/partlycloudy.png'
 import { Link } from 'expo-router'
 import GradientText from '../GradientText'
-import { CalendarDaysIcon, ArrowRightCircleIcon, ChevronDownIcon, ChevronUpIcon } from 'react-native-heroicons/solid'
+import { CalendarDaysIcon, ArrowRightCircleIcon, ChevronDownIcon, ChevronUpIcon, MapPinIcon, MagnifyingGlassIcon } from 'react-native-heroicons/solid'
 import LottieView from 'lottie-react-native'
+import Greeting from './components/Greeting'
 
 const CurrentWeather = () => {
     const theme = useTheme()
     const [isShown, setIsShown] = useState(true)
+    const [showSearch, toggleShowSearch] = useState(false)
+    const [locations, setLocations] = useState([1, 2, 3])
 
     return (
         <>
+            <Header>
+                <SearchContainer variant={showSearch} >
+                    {
+                        showSearch ? (
+                            <InputText placeholder='Search City' placeholderTextColor={'lightgray'} />
+                        ) : (
+                            <Greeting />
+                        )}
+                    <MagnifyingIconContainer onPress={() => toggleShowSearch(!showSearch)}>
+                        <MagnifyingGlassIcon color='white' size={25} />
+                    </MagnifyingIconContainer>
+                </SearchContainer >
+                {locations.length > 0 && showSearch ? (
+                    <Locations>
+                        {locations.map((loc, index) => {
+                            return (
+                                <LocationList key={index}>
+                                    <MapPinIcon color='black' size={25} />
+                                    <Text>London, United Kingdom</Text>
+                                </LocationList>
+                            )
+                        })}
+                    </Locations>
+                ) : null}
+            </Header>
+
             {/* Current weather */}
             <Container>
                 <Location>London,
@@ -141,6 +170,64 @@ const CurrentWeather = () => {
 
 export default CurrentWeather
 
+const Header = styled.View`
+        position: relative;
+        margin:0 8px;
+        z-index: 50;
+`
+const SearchContainer = styled.View`
+    color: white;
+    border-radius: 32px;
+    padding-left: 16px;
+    margin: 8px 0px;
+    display: flex;
+    flex-direction: row;
+    align-items: top;
+    height: 56px;
+    ${(props) => {
+        switch (props.variant) {
+            case true:
+                return css`
+                    border: 1px solid white;
+                    background-color: ${(props) => props.theme.bgWhite(0.1)};
+                    justify-content:flex-end;
+                `;
+            case false:
+                return css`
+                    justify-content:space-between;
+                `
+        }
+    }}
+`
+const InputText = styled.TextInput`
+    color: white;
+    font-size: 24px;
+    flex-grow: 1;
+`
+const MagnifyingIconContainer = styled.TouchableOpacity`
+    background-color: ${(props) => props.theme.bgWhite(0.2)};
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    padding: 12px;
+    margin:2px;
+`
+const Locations = styled.View`
+    position: absolute;
+    top:72px;
+    background-color:#f0f0f0;
+    width: 100%;
+    border-radius: 24px;
+`
+const LocationList = styled.TouchableOpacity`
+    display: flex;
+    flex-direction: row;
+    gap:10px;
+    padding: 12px;
+    align-items: center; 
+    
+   
+`
 const WeatherImage = styled.Image`
     width: 220px;
     height: 220px;
